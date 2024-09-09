fcdc8781b9c71a323cc8efcb79b8a85c5798dc15
Niels Jansen <nielswoodkiwak@gmail.com>
2024-09-09 20:37:10 +0200

    Finished assignment

---
+++ theme/console-output/Program.cs
@@ -0,0 +1,12 @@
+string symbol = "*";
+
+Console.WriteLine($"{symbol}{symbol}{symbol}{symbol}{symbol}");
+Console.WriteLine($"{symbol}   {symbol}");
+Console.WriteLine($"{symbol}   {symbol}");
+Console.WriteLine($"{symbol}   {symbol}");
+Console.WriteLine($"{symbol}{symbol}{symbol}{symbol}{symbol}");
+
+
+Console.Write("Enter your full name: ");
+string? name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
---
+++ theme/console-output/README.md
@@ -0,0 +1,10 @@
+# Assignment: Theme - Console Output
+
+## Opdracht 1
+Schrijf een programma dat hello world uitprint.
+
+## Opdracht 2
+Schrijf stuk code dat je naam uitprint.
+
+## Opdracht 3
+Schrijf een stuk code dat een vierkant van 5 bij 5 van sterretjes afdrukt zoals in onderstaande blok output.
