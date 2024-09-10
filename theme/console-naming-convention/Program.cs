9f49baa084a1f13c629d208aa9c9b4ece618301e
Niels Jansen <nielswoodkiwak@gmail.com>
2024-09-10 19:14:40 +0200

    Finished naming convention assignment

---
+++ theme/console-naming-convention/Program.cs
@@ -0,0 +1,23 @@
+﻿// Assignment 1: Improving the Naming Convention 
+public interface IDisplay
+{
+    void display();
+}
+
+
+public class Customer : IDisplay
+{
+    public int id;
+    public string name;
+
+    public Customer(int a, string b)
+    {
+        id = a;
+        name = b;
+    }
+
+    public void display()
+    {
+        Console.WriteLine("ID: " + id + ", Name: " + name);
+    }
+}
