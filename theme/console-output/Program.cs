bcad90cddbe1dc529230327881e618a16a4a6459
Niels Jansen <niels.jansen@schippers.eu>
2024-09-10 15:41:06 +0200

    Console App Test created in the classroom

--- theme/console-output/Program.cs
+++ theme/console-output/Program.cs
@@ -10,3 +10,4 @@ Console.WriteLine($"{symbol}{symbol}{symbol}{symbol}{symbol}");
 Console.Write("Enter your full name: ");
 string? name = Console.ReadLine();
 Console.WriteLine($"Hello, {name}!");
+
