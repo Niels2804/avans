5360b2cd5d053c97b556820de6f8710334aacf97
Niels Jansen <niels.jansen@schippers.eu>
2024-09-26 12:33:04 +0200

    Base setup for 3 random games

---
+++ theme/console-three-games/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading;
+
+namespace Checkbox {
+    public class Program : Games {
+        private static readonly string _checkboxHeadline = "Select one of the following options:";            
+        private static readonly string[] _options = ["Galgje", "Mastermind", "Boter, kaas en eieren"];
+
+        // Main Game menu
+        private static void Main() {
+
+            // Initialize checkboxes 
+            Checkbox checkbox = new Checkbox(_checkboxHeadline, _options); // In this case, max 1 option can be selected
+            CheckboxReturn[] response = checkbox.Select();
+            Console.Clear();
+            
+            foreach (CheckboxReturn checkboxReturn in response)
+            {
+                SelectedGameOption = checkboxReturn.Index;
+                Console.WriteLine($"U heeft de game optie {checkboxReturn.Option} geselecteerd. De game wordt nu ingeladen...");
+                System.Threading.Thread.Sleep(1500);
+                Console.Clear();
+            }
+            Games.Initialize(); // initializing game
+        }
+    }
+
+    public class Games
+    {
+        protected static int SelectedGameOption {get; set;}
+
+        // Constructor
+        protected static void Initialize() {
+            switch(SelectedGameOption) {
+                case 0:
+                    Galgje.Initialize();
+                    break;
+                case 1:
+                    break;
+                case 2: 
+                    break;
+                default: 
+                    Console.WriteLine("No game options are selected, the game will ending now");
+                    System.Threading.Thread.Sleep(3000);
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        // Game 1: Galgje
+        protected class Galgje {
+            protected static List<string> words { get; set; }
+
+            public static void Initialize()
+            {
+                try {
+                    using (StreamReader File = new StreamReader("randomWords.json")) {   
+                        string json = File.ReadToEnd();
+                        var jsonData = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(json);
+
+                        if (jsonData != null && jsonData.ContainsKey("words")){
+                            words = jsonData["words"];
+                            foreach (string word in words)
+                            {
+                                Console.WriteLine(word);
+                            }
+                        } else {
+                            Console.WriteLine("No words for the game \"Galgje\" found. The game will be ended...");
+                            System.Threading.Thread.Sleep(3000);
+                            Environment.Exit(0);
+                        }     
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine($"An error occurred while initializing Galgje game: {e.Message}");
+                }
+            }
+        }        
+    }
+}
+
+// Mastermind
+
+// Boter, kaas en eieren
--- theme/console-variables-expressions/Program.cs
+++ theme/console-variables-expressions/Program.cs
@@ -1,51 +1,7 @@
 ﻿// assignment 1
-List<string> tableItems = new List<string>{ "DIN formaat", "Lengte (mm)", "Breedte (mm)", "Oppervlakte (mm2)"};
-
-int DIN = 0;
-int lengte = 1189;
-int breedte = 841;
-
-foreach(string tableItem in tableItems)
-{
-    Console.Write(tableItem + "\t");
+float randomNumber = new Random().Next(0, 10);
+if (randomNumber % 2 == 0) {
+    randomNumber /= 2;
 }
-Console.WriteLine();
-
-for(int i = 0; i < 14; i++) {
-    Console.Write($"A{DIN}\t\t");
-    Console.Write($"{lengte}mm\t\t");
-    Console.Write($"{breedte}mm\t\t");  
-    Console.WriteLine($"{lengte * breedte}mm2\t\t");
-
-    lengte = breedte;
-    breedte = lengte / 2;
-    DIN++;
-}
-
 // assignment 2
-List<string> tableItems2 = new List<string>{ "Invoer", "Uitvoer (aantal klinkers)"};
-List<string> randomZinnen = new List<string>{ 
-    "De kat zit op de mat te slapen", 
-    "De hond rent door het park", 
-    "De vogel vliegt in de lucht", 
-    "De vis zwemt in het water", 
-    "De muis kruipt door het huis"
-};
-
-foreach(string tableItem in tableItems2)
-{
-    Console.Write(tableItem + "\t\t\t\t\t");
-}
-Console.WriteLine();
-
-foreach(string zin in randomZinnen) {
-    int index = 0;
-    foreach(char c in zin) {
-        if(c == 'a' || c == 'o' || c == 'u' || c == 'e' || c == 'i') {
-            index++;
-        }
-    }
-    Console.WriteLine($"{zin}\t\t\t {index}");
-}
-
 
