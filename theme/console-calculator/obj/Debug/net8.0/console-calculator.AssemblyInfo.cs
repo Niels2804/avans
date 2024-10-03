d7e7c45bedcfbad4a1411365bba54a098cd81c2a
Niels Jansen <niels.jansen@schippers.eu>
2024-10-03 11:30:55 +0200

    rename project

--- theme/console-calculator/obj/Debug/net8.0/console-calculator.AssemblyInfo.cs
+++ theme/console-calculator/obj/Debug/net8.0/console-calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("console-calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e363b22cc678edd53d9e87d4a0db370d3a32e85c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84b7581aaef5fa6cea5eb1fa27c7066daa5ad00")]
 [assembly: System.Reflection.AssemblyProductAttribute("console-calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("console-calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- theme/console-three-games/obj/Debug/net8.0/console-three-games.AssemblyInfo.cs
+++ theme/console-three-games/obj/Debug/net8.0/console-three-games.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("console-three-games")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c044632357554770c85eee94a957374454e1a4ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84b7581aaef5fa6cea5eb1fa27c7066daa5ad00")]
 [assembly: System.Reflection.AssemblyProductAttribute("console-three-games")]
 [assembly: System.Reflection.AssemblyTitleAttribute("console-three-games")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
