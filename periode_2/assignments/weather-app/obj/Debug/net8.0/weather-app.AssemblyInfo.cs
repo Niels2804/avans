ab98e7ba9886b7d58dc66e02488e2f0032981451
Niels Jansen <nielswoodkiwak@gmail.com>
2024-11-25 00:40:06 +0100

    caching

--- periode_2/assignments/weather-app/obj/Debug/net8.0/weather-app.AssemblyInfo.cs
+++ periode_2/assignments/weather-app/obj/Debug/net8.0/weather-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weather-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db593e506f3726571baadf2549a7eed7ac2bd23a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6bd4a81b306ccd22c1b7983d8589589c8b69f62")]
 [assembly: System.Reflection.AssemblyProductAttribute("weather-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weather-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
