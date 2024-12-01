dbc915112827f1803bc8f7e166614843da9627a4
Niels Jansen <niels.jansen@schippers.eu>
2024-12-01 16:21:19 +0100

    commit MQTTFirstAttempt branch

---
+++ periode_2/assignments/MQTTFirstAttempt/Program.cs
@@ -0,0 +1,9 @@
+﻿
+if(await SendService.SendMessage("jow", "jow")) 
+{
+    Console.WriteLine("Bericht is verzonden");
+} else {
+    Console.WriteLine("Bericht is niet verzonden");
+}
+
+Console.ReadLine();
